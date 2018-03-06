b2ee8dfdcc0eca3f09d7d654480f6a590f5cde36
Masami96 <crazyazianfool@gmail.com>
2018-03-05 19:54:51 -0800

    initial endpoints created

--- blog-server/app.js
+++ blog-server/app.js
@@ -16,11 +16,26 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+/*
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+*/
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.get('/blog/:username/:postid', function(req, res) {
+  res.render('pages/blog');
+});
+
+app.get('/blog/:username', function(req, res)) {
+  res.render('pages/blog');
+});
+
+/*
 
 app.use('/', index);
 app.use('/users', users);
@@ -44,3 +59,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+*/
+app.listen(3000);
+console.log('3000 babbbbyyyy');
