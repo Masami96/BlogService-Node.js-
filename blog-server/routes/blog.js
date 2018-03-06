a1262de8122929fabef0b13a488e1056ddac8778
Masami96 <crazyazianfool@gmail.com>
2018-03-06 12:24:36 -0800

    implementing second part of blog page

--- blog-server/routes/blog.js
+++ blog-server/routes/blog.js
@@ -8,7 +8,38 @@ var commonmark = require('commonmark');
 
 /* GET home page. */
 router.get('/:username', function(req, res, next) {
-  res.render('pages/blog', { username: req.params.username });
+  var reader = new commonmark.Parser();
+  var writer = new commonmark.HtmlRenderer();
+
+  var user = req.params.username;
+
+  var mydb = req.app.get('db');
+  var blogdb = mydb.db('BlogServer');
+
+  blogdb.collection('Posts').find({username: user}, {}).toArray(function(err, result) {
+    if (err) throw err;
+    var titleArray = [];
+    var bodyArray = [];
+    for (var i = 0; i < result.length; i++){
+      var parsed = reader.parse(result[i].title);
+      var resultTitle = writer.render(parsed);
+      var parsed = reader.parse(result[i].body);
+      var resultBody = writer.render(parsed);
+      titleArray.push(resultTitle);
+      bodyArray.push(resultBody);
+    }
+    console.log(titleArray);
+    console.log(bodyArray);
+
+    res.render('pages/blog1', {
+      username: user,
+      titles: titleArray,
+      bodies: bodyArray
+     });
+
+  });
+
+  //res.render('pages/blog1', { username: req.params.username });
 });
 
 router.get('/:username/:postid', function(req, res, next) {
@@ -21,7 +52,7 @@ router.get('/:username/:postid', function(req, res, next) {
 
   var mydb = req.app.get('db');
   var blogdb = mydb.db('BlogServer');
-  
+
   blogdb.collection('Posts').findOne({
       postid: pid,
       username: user
